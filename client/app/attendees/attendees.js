b9d8e298e45611e455f0cfd4d5745a01e94ebb25
Luke Wilson <wilson.palooza@gmail.com>
2016-10-17 20:05:45 -0700

    - Fixes attendee count variable / function (it wasn't async)

--- client/app/attendees/attendees.js
+++ client/app/attendees/attendees.js
@@ -1,6 +1,7 @@
 angular.module('networker.attendees', [])
 .controller('AttendeesController', function($scope, Attendees){
   $scope.test = "hello";
+  Attendees.getCount().then(function(c) {$scope.count = c;});
   $scope.addAttendee = function(attendee) {
     Attendees.addAttendee(attendee);
   }
