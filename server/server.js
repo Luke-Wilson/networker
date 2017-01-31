120121eea9923181685186d36e2204764682f47a
Luke Wilson <wilson.palooza@gmail.com>
2017-01-30 18:18:31 -0800

    Hide database credentials

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var app      = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var dbAddress = require('./config/database.js');
 
 var app = express();
 
@@ -14,19 +15,19 @@ app.use(bodyParser.json());
 // connect to mongo database named "networker"
 //mongoose.connect('mongodb://localhost/networker');
 // mongoose.connect(process.env.MONGOLAB_URI);
-mongoose.connect('mongodb://test:testy@ds139735.mlab.com:39735/networkerdb');
+mongoose.connect(dbAddress);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("db connected")
+  console.log("db connected");
 });
 
 // configure our server with all the middleware and routing
 // require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-app.listen(process.env.PORT || 8080, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('server is listening');
 });
 
